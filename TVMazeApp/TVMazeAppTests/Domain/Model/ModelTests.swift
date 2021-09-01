896a3d2987391b57dda82495945bab633438f1b9
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-09-01 18:21:34 -0300

    Add Model Protocol

---
+++ TVMazeApp/TVMazeAppTests/Domain/Model/ModelTests.swift
@@ -0,0 +1,40 @@
+import XCTest
+@testable import TVMazeApp
+
+class ModelTests: XCTestCase {
+
+    func testModel_codable_ShouldEncodeAndDecodeMockModel() throws {
+        // Arrange
+        let sut = MockModel(value: "string")
+        let decoder = JSONDecoder()
+
+        // Act
+        guard let data = sut.toData() else {
+            XCTFail("Should return a valid data.")
+            return
+        }
+        let decodedValue = try decoder.decode(MockModel.self, from: data)
+
+        // Assert
+        XCTAssertEqual(sut, decodedValue)
+    }
+
+    func testModel_toJson_ShouldReceiveADictionary() throws {
+        // Arrange
+        let sut = MockModel(value: "string")
+
+        // Act
+        guard let dictionary = sut.toJson() else {
+            XCTFail("Should return a dictionary.")
+            return
+        }
+
+        // Assert
+        XCTAssertTrue(dictionary.keys.contains("value"))
+        XCTAssertEqual(dictionary["value"] as? String, sut.value)
+    }
+
+    private struct MockModel: Model {
+        let value: String
+    }
+}
