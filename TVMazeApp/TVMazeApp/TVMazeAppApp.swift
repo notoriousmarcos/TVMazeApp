62660985ad1665d1ad984fa8d952524ed59863c6
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-09-01 18:14:06 -0300

    First commit

---
+++ TVMazeApp/TVMazeApp/TVMazeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAppApp.swift
+//  TVMazeApp
+//
+//  Created by marcos.brito on 01/09/21.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
