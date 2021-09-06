0c8c76226ce05437e1ff699a41a666628ed063fd
Marcos Vinicius Brito <marcosvcbrito@gmail.com>
2021-09-06 09:52:09 -0300

    Updating tests

--- TVMazeApp/TVMazeAppTests/Main/Factory/ShowsViewFactoryTests.swift
+++ TVMazeApp/TVMazeAppTests/Main/Factory/ShowsViewFactoryTests.swift
@@ -5,6 +5,7 @@
 //  Created by marcos.brito on 05/09/21.
 //
 
+import Combine
 import SwiftUI
 import XCTest
 @testable import TVMazeApp
@@ -17,7 +18,19 @@ class ShowsViewFactoryTests: XCTestCase {
             fetchShowsByPageUseCase: RemoteFetchShowByPageUseCase(httpClient: NativeHTTPClient()))
 
         // Act
-        let view = sut.make()
+        let view = sut.make(openShowAction: { show in
+            ShowDetailView(
+                viewModel: ShowDetailViewModel(
+                    show: show,
+                    fetchShowById: { _ -> AnyPublisher<Show, DomainError> in
+                        return Mocks.makeSuccessPublisher(forValue: Mocks.show)
+                    },
+                    fetchEpisodesByShow: { _ -> AnyPublisher<[Episode], DomainError> in
+                        return Mocks.makeSuccessPublisher(forValue: [Mocks.episode])
+                    }
+                )
+            )
+        })
 
         // Assert
         XCTAssertNotNil(view)
